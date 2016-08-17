e2636745ba7878dbbdcfe4853b758f979d4d4a5d
Juwon <appkr@appkr.local>
2016-08-18 01:25:31 +0900

    move view()->share() to base controller

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -10,6 +10,7 @@ class CommentsController extends Controller
 {
     public function __construct()
     {
+        parent::__construct();
         $this->middleware('auth', ['except' => 'index']);
     }
 
